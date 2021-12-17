381ef038cac0e4b91c6ce6903464264d8bb91349
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-12-17 10:04:47 +0530

    Home component started

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.css';
-import SearchIcon from "@material-ui/icons/Search"
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
 
 function Header() {
     return (
@@ -25,7 +27,7 @@ function Header() {
                         Returns
                     </span>
                     <span className='header__optionLineTwo'>
-                        & orders
+                        & Orders
                     </span>
                 </div>
                 <div className="header__option">
@@ -34,6 +36,13 @@ function Header() {
                     </span>
                     <span className='header__optionLineTwo'>
                         Prime
+                    </span>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className='header__optionLineTwo header__basketCount'>
+                        0
                     </span>
                 </div>
             </div>
