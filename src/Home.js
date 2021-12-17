df4facba683d06f1d389d6103875eca009d45742
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-12-17 11:18:45 +0530

    Product component started

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
         <div className='home'>
             <div className="home__container">
-                <img className='home__image' src="https://m.media-amazon.com/images/I/71G7vKHnFZL._SX3000_.jpg" alt="Banner" />
+                <div className="home__imageContainer">
+                    <img className='home__image' src="https://m.media-amazon.com/images/I/71G7vKHnFZL._SX3000_.jpg" alt="Banner" />
+                </div>
+                <div className="homr">
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+                <div className="home__row">
+                    {/* Product */}
+                </div>
             </div>
         </div>
     )
