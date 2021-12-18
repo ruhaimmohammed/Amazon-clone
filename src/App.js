d5c0a9721eeb6cde106562416dfca4758b2f8fb9
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-12-19 02:12:09 +0530

    Subtotal component startes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Main from "./Main"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from './Checkout';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
+      <Header />
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Home />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </div>
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Header from './Header'
+import "./Checkout.css";
+import Header from './Header';
 
 function Checkout() {
     return (
         <div className='checkout'>
-            <Header />
+            <div className="checkout__left">
+                <h2 className="checkout__title">
+                    Shopping Basket
+                </h2>
+            </div>  
+            <div className="checkout__right">
+                <h2>The subtotal will be here</h2>
+            </div>
         </div>
     )
 }
 
 export default Checkout
+ 
