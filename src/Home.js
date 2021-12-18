159caaa3ebc1afe74ef47a283397c5158031e75e
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-12-18 23:17:14 +0530

    Completed main page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css';
 import Product from './Product';
+import "./TV.mp4";
 
 function Home() {
     return (
@@ -9,17 +10,53 @@ function Home() {
                 <div className="home__imageContainer">
                     <img className='home__image' src="https://m.media-amazon.com/images/I/71G7vKHnFZL._SX3000_.jpg" alt="Banner" />
                 </div>
-                <div className="homr">
-                    {/* Product */}
-                    {/* Product */}
+                <div className="home__row">
+                    <Product 
+                    id="1"
+                    title= "Zero to one"
+                    price={250}
+                    image="https://m.media-amazon.com/images/I/71m-MxdJ2WL._AC_UY327_FMwebp_QL65_.jpg"
+                    rating={5}
+                    />
+                    <Product
+                    id="2"
+                    title= "HP 15 AMD Ryzen 5 5500U 15.6 inch(39.6 cms) FHD Laptop (8GB RAM/512GB SSD/Windows 10/MS Office/1.69 kg), 15s-eq2040AU"
+                    price={56999}
+                    image="https://m.media-amazon.com/images/I/81SdJG3OUjL._SX450_.jpg"
+                    rating={5}
+                     />
                 </div>
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
+                    <Product 
+                    id="3"
+                    title= "Nike Air Force 1 GORE-TEX"
+                    price={9995}
+                    image="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/kkeylxui0asbndztnwux/air-force-1-gore-tex-shoes-xz207L.png"
+                    rating={4}
+                    />
+                    <Product 
+                    id="4"
+                    title= "OnePlus 2 (Sandstone Black, 64GB)"
+                    price={22999}
+                    image="https://content.oneplus.net/skin/frontend/oneplus2015/default/images/feature/two/impress-img.png"
+                    rating={5}
+                    />
+                    <Product 
+                    id="5"
+                    title="boAt Bassheads 100 in Ear Wired Earphones with Mic(Furious Red)"
+                    price={329}
+                    image="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSd1n-wnE93le8f5YDlNOPoDbYphavUMJL-cwK8zlxPosX4Rx_b0ngIRbsyX6pt6zm1HQ0y4lzj&usqp=CAc"
+                    rating={4}
+                    />
                 </div>
                 <div className="home__row">
-                    {/* Product */}
+                    <Product 
+                    id="6"
+                    title= "Samsung 138 cm (55 inches) Crystal 4K Series Ultra HD Smart LED TV UA55AUE60AKLXL (Black) (2021 Model)"
+                    price={61990}
+                    video="https://www.youtube.com/watch?v=GRFKeCRoZco"
+                    rating={5}/>               
+
                 </div>
             </div>
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import './Product.css';
+import ReactPlayer from 'react-player'
+import "./TV.mp4"; 
 
-function Product() {
+function Product({ title, image, video, price, rating }) {
+    let picture;
+    if(image){
+        picture = <img src={ image } alt={ title } /> ;
+    }else{
+        picture = <ReactPlayer 
+                    url={ video }
+                    playing={ true }
+                    muted={ true }
+                    loop={ true }/> ;
+    }
+
     return (
         <div className='product'>
             <div className="product__info">
-                <p>Zero to one</p>
+                <p>{ title }</p>
                 <p className='product__price'>
-                    <small>$</small>
-                    <strong>19.99</strong>
+                    <small>₹</small>
+                    <strong> { price }</strong>
                 </p>
+                <div className="product__rating">
+                   { Array(rating).fill().map((_, i) =>( 
+                    <p>⭐</p>
+                   ))}
+                </div>
             </div>
+            <div className="product__imageContainer">
+                { picture }
+                
+            </div>
+            <button className='add__product'>Add to basket</button>
         </div>
     )
 }
