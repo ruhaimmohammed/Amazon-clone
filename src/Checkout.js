8127f4000fa9517ef68ca0af420592094fbd4dec
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-12-21 02:55:11 +0530

    Checkout product retreiving from data layer and design done

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import "./Checkout.css";
-import Header from './Header';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 function Checkout() {
+    const[{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='checkout'>
             <div className="checkout__left">
                 <h2 className="checkout__title">
                     Shopping Basket
                 </h2>
+
+                {basket.map(item => (
+                <CheckoutProduct 
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    rating={item.rating}
+                />
+                ))}
             </div>  
             <div className="checkout__right">
                 <Subtotal />
