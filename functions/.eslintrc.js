e001caa88e44a0b436fa60e39b510672ef4a4f27
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2022-01-13 03:44:55 +0530

    CLoud functions deployed

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,3 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+  parser: "@babel/eslint-parser",
 };
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-3421f/us-central1/api' // THE API URL
+    baseURL: 'https://us-central1-clone-3421f.cloudfunctions.net/api' // THE API URL
 })
 
-export default instance;
+export default instance; 
